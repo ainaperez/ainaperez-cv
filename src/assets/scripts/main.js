0451328d40e2187981da8f076ac7250f6336e279
Aina Perez Serra <aperezserr@uoc.edu>
2021-10-21 13:38:37 +0200

    first commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,39 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+
+/* ----- Burger Menu function ------*/
+
+const burger = document.querySelector(".burger"); 
+const menu = document.querySelector(".nav-ul");
+const navLink = document.querySelectorAll(".nav-li");
+
+//open menú on burger click 
+
+burger.addEventListener("click", burgerToggle); 
+
+function burgerToggle(){
+  menu.classList.toggle("active");
+  burger.classList.toggle("active");
+}
+
+//close menú on Link click
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  menu.classList.remove("active");
+  burger.classList.remove("active");  
+}
+
+
+/* ----- Circle rotation on scroll ------ */
+
+window.onscroll = function () {
+  scrollRotate();
+};
+
+function scrollRotate() {
+  const circle = document.querySelector(".circle-img");
+  circle.style.transform = "rotate(" + window.pageYOffset/6 + "deg)";
+}
